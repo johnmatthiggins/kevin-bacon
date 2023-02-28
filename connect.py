e5e5a4ee7a7c445e52593bb2042c74e738a66c9f
John Higgins <johnmatthiggins@gmail.com>
2023-02-27 16:35:18 -0800

    Finished writing data retrieving script

---
+++ connect.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+import pandas as pd
+
+ACTORS_FILE=
+
+def main():
+    df = pd.read_csv(, sep='\t')
+    print(df)
+
+
+if __name__ == '__main__':
+    main()
--- get_data.sh
+++ get_data.sh
@@ -1,19 +1,24 @@
-#!/usr/bin/env zsh
+#!/usr/bin/env sh
+# clean from previous
+rm *.gz
+
 ACTORS_FILE='name.basics.tsv.gz'
 MOVIES_FILE='title.basics.tsv.gz'
 BASE_URL='https://datasets.imdbws.com/'
 ACTORS_URL="$(printf '%s%s' $BASE_URL $ACTORS_FILE)"
 MOVIES_URL="$(printf '%s%s' $BASE_URL $MOVIES_FILE)"
 
-curl $ACTORS_URL -o $ACTORS_FILE
-curl $MOVIES_URL -o $MOVIES_FILE
+wget $ACTORS_URL
+wget $MOVIES_URL
 
 gzip -dk $ACTORS_FILE
 gzip -dk $MOVIES_FILE
 
-if test -f "data"; then
+if test -f data/; then
+    echo 'Found data/ folder'
 else
-    mkdir "data"
+    echo 'Creating data/ folder'
+    mkdir data
 fi
 
 # move them all to the data file which is .gitignore'd
