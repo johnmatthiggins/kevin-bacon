430927f22bd04748ab1470ebf24fc024830081fa
John Higgins <johnmatthiggins@gmail.com>
2023-03-01 23:33:30 -0800

    Added stuff

--- src/cpp/cleaner.h
+++ src/cpp/cleaner.h
@@ -2,7 +2,9 @@
 #define CLEANER_H
 
 #include <string>
+#include <cstdint>
 
-bool parse_line(std::string& line, int* source, int* destination);
+bool parse_line(std::string& line, int32_t* source, int32_t* destination);
+std::string to_four_byte_str(int32_t num);
 
 #endif
